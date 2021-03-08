b1eb6b021c46525edf1113ce3d3fadef150da7a8
파랑이 <parangee9706@gmail.com>
2021-03-08 20:02:06 +0900

    backup

---
+++ src/structures/client.ts
@@ -0,0 +1,11 @@
+import { AkairoClient } from "discord-akairo";
+// @ts-ignore
+import config from '../config.json'
+
+export default class QseClient extends AkairoClient {
+    config = config
+
+    constructor() {
+        super()
+    }
+}
