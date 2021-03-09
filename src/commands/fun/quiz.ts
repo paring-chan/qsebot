49838efd3af9b0cebfa60675606d4256fd236e73
파랑이 <parangee9706@gmail.com>
2021-03-09 17:47:30 +0900

    feat: change to reply

--- src/commands/fun/quiz.ts
+++ src/commands/fun/quiz.ts
@@ -16,19 +16,19 @@ export default class Quiz extends Command {
         const embed = new MessageEmbed()
         embed.setTitle('퀴즈')
         embed.setDescription(quiz.question)
-        const m = await msg.channel.send(embed)
+        const m = await msg.reply(embed)
         await m.react(client.config.reactions.yes)
         await m.react(client.config.reactions.no)
         const res = await m.awaitReactions((reaction, user) => [client.config.reactions.yes, client.config.reactions.no].includes(reaction.emoji.id) && user.id === msg.author.id, {
             time: 30000,
             max: 1
         })
-        if (!res.size) return msg.channel.send('시간초과')
+        if (!res.size) return msg.reply('시간초과')
         const reaction = res.first()!
         if (reaction.emoji.id === client.config.reactions.yes && quiz.answer) {
-            return msg.channel.send(quiz.correct)
+            return msg.reply(quiz.correct)
         } else {
-            return msg.channel.send(quiz.incorrect)
+            return msg.reply(quiz.incorrect)
         }
     }
 }
