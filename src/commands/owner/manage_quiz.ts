58ffb3fc2cfd3bdcc24e0f3f8651f01f6329a179
파랑이 <parangee9706@gmail.com>
2021-03-10 15:16:53 +0900

    feat: remove yargs dependent code

--- src/commands/owner/manage_quiz.ts
+++ src/commands/owner/manage_quiz.ts
@@ -6,11 +6,19 @@ export default class ManageQuiz extends Command {
     constructor() {
         super('owner__manage_quiz', {
             ownerOnly: true,
-            aliases: ['퀴즈관리']
+            aliases: ['퀴즈관리'],
+            args: [
+                {
+                    id: 'op',
+                    default: '',
+                    type: 'string',
+                    match: 'rest'
+                }
+            ]
         })
     }
 
-    async exec(msg: Message) {
+    async exec(msg: Message, {op}: any) {
         const client = this.client as QseClient
         const {prisma} = client
     }
