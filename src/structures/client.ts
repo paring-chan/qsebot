51cd3229c9768cd5b901950d7d54024161224e48
파랑이 <parangee9706@gmail.com>
2021-03-10 14:50:14 +0900

    feat: prisma client

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -4,6 +4,7 @@ import Discord, {Team, User} from "discord.js";
 import config from '../../config.json'
 import Dokdo from "dokdo";
 import path from "path";
+import { PrismaClient } from "@prisma/client";
 
 export default class QseClient extends AkairoClient {
     config = config
@@ -17,6 +18,8 @@ export default class QseClient extends AkairoClient {
     listenerHandler = new ListenerHandler(this, {
         directory: path.join(__dirname, '../listeners')
     })
+
+    prisma = new PrismaClient()
 
     constructor() {
         super({
