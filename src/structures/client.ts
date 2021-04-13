4912ed5fc86f7ab2b6fbbc7574c572380221bc81
파랑이 <parangee9706@gmail.com>
2021-04-13 17:54:33 +0900

    feat: patch client module

--- src/structures/client.ts
+++ src/structures/client.ts
@@ -1,56 +1,28 @@
-import {AkairoClient, CommandHandler, ListenerHandler} from "discord-akairo";
 import Discord, {Team, User} from "discord.js";
 // @ts-ignore
 import config from '../../config.json'
 import Dokdo from "dokdo";
-import path from "path";
 import mongoose from "mongoose";
+import {CommandClient} from "@pikostudio/command.ts";
 
-export default class QseClient extends AkairoClient {
+export default class QseClient extends CommandClient {
     config = config
-
-    commandHandler = new CommandHandler(this, {
-        prefix: config.prefix,
-        directory: path.join(__dirname, '../commands'),
-        automateCategories: true
-    })
-
-    listenerHandler = new ListenerHandler(this, {
-        directory: path.join(__dirname, '../listeners')
-    })
 
     constructor() {
         super({
             restTimeOffset: 0,
             intents: Discord.Intents.ALL
+        }, {
+            prefix: config.prefix,
+            owners: 'auto'
         })
-
-        this.listenerHandler.setEmitters({
-            client: this,
-            commandHandler: this.commandHandler,
-            listenerHandler: this.listenerHandler
-        })
-
-        this.listenerHandler.loadAll()
-
-        this.commandHandler.loadAll()
 
         mongoose.connect(config.db).then(() => this.login(config.token)).then(async () => {
-            const app = await this.fetchApplication()
-
-            let owner: string[] = []
-
-            if (app.owner instanceof User) {
-                owner = [app.owner.id]
-            } else if (app.owner instanceof Team) {
-                owner = app.owner.members.map(r=>r.id)
-            }
-            this.ownerID = owner
             const dokdo = new Dokdo(this, {
                 noPerm(msg: Discord.Message): any {
                     msg.react(config.reactions.noPerm)
                 },
-                owners: owner,
+                owners: this.owners,
                 prefix: config.prefix
             })
             this.on('message', msg => dokdo.run(msg))
