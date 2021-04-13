a7b158257e6316990c878cb00ebe5a17987474d1
파랑이 <parangee9706@gmail.com>
2021-04-13 20:23:29 +0900

    feat: add some classes and enable decorator

---
+++ src/modules/dev.ts
@@ -0,0 +1,44 @@
+import QseClient from '../structures/client'
+import { command, listener, ownerOnly } from '@pikostudio/command.ts'
+import { Message } from 'discord.js'
+import PatchedModule from '../structures/PatchedModule'
+import chalk from 'chalk'
+
+class Dev extends PatchedModule {
+    constructor(public client: QseClient) {
+        super(__filename)
+    }
+
+    @ownerOnly
+    @command()
+    async reload(msg: Message) {
+        const modules = this.client.registry.modules
+            .filter((x) => x.__path.startsWith(__dirname))
+            .values()
+        let result = '```\n'
+        let success = 0
+        let failed = 0
+        for (const module of modules) {
+            try {
+                await this.client.registry.reloadModule(module)
+                result += `✅ ${module.constructor.name}\n`
+                success++
+            } catch {
+                result += `🚫 ${module.constructor.name}\n`
+                failed++
+            }
+        }
+        result += `\`\`\`\n${success} successful, ${failed} failed.`
+        await msg.reply(result)
+        console.log(`=========== ${chalk.cyan('[INFO]')} RELOADED ===========`)
+    }
+
+    @listener('commandError')
+    commandError(err: Error) {
+        console.log(err)
+    }
+}
+
+export function install(client: QseClient) {
+    return new Dev(client)
+}
