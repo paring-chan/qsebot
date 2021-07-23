40bf57ecec26d3a74b18c64bbd91b4e83fc122ec
pikokr <parangee9706@gmail.com>
2021-07-23 14:36:42 +0900

    update youtube feature

--- src/youtube.ts
+++ src/youtube.ts
@@ -15,8 +15,15 @@ export default function initYoutube(client: QseClient) {
     notifier.setup()
 
     notifier.on('notified', (data: any) => {
-        ;(client.guilds.cache.get(config.guild)?.channels.cache.get(config.youtube.channel) as TextChannel|undefined)?.send(`큐튜브 업로드\n${data.video.link}`)
+        if (data.channel.id === config.youtube.id) {
+            ;(client.guilds.cache.get(config.guild)?.channels.cache.get(config.youtube.channel) as TextChannel|undefined)?.send(`큐튜브 업로드\n${data.video.link}`)
+        } else if (config.youtube.extraChannels.includes(data.channel.id)) {
+            ;(client.guilds.cache.get(config.guild)?.channels.cache.get(config.youtube.extraChannel) as TextChannel|undefined)?.send(`ㄴㅇㅊㅇ\n${data.video.link}`)
+        }
     })
 
     notifier.subscribe(config.youtube.id)
+    for (const i of config.youtube.extraChannels) {
+        notifier.subscribe(i)
+    }
 }
